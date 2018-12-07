272cd41d33035208d5769fc65ea8012c8dbb8f2d
elaygall <45111194+elaygall@users.noreply.github.com>
2018-12-07 15:32:42 +0300

    Add files via upload

---
+++ CounterNuc.py
@@ -0,0 +1,11 @@
+dict = {'A': 0, 'T': 0, 'G': 0, 'C': 0}
+
+data = open('rosalind_dna.txt')
+s = data.readline()
+
+for i in s:
+    if i in dict:
+        dict[i] = dict[i] + 1
+
+for k in sorted(dict.keys()):
+    print (dict[k], end=' ')

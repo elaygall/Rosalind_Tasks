406c9da0524bb1eb26e3ce8cf6af84289ec8a8bd
Ilia Galkin <45111194+elaygall@users.noreply.github.com>
2018-12-18 02:52:00 +0300

    Update CounterNuc.py

--- CounterNuc.py
+++ CounterNuc.py
@@ -1,8 +1,7 @@
-
-dict = {'A': 0, 'T': 0, 'G': 0, 'C': 0}
-
 data = open('rosalind_dna.txt')
 s = data.readline()
+
+dict = {'A': 0, 'T': 0, 'G': 0, 'C': 0}
 
 for i in s:
     if i in dict:
@@ -10,4 +9,3 @@ for i in s:
 
 for k in sorted(dict.keys()):
     print(dict[k], end=' ')
-    
